86ffd2ef5892d8775e3372d0110ea0a4c76dda8a
Mikhail Tripolitov <tripolitov@gmail.com>
2020-10-02 14:10:59 +0300

    Solution for Is Subsequence using Binary Search

--- Train.Algorithms/IsSubsequence.cs
+++ Train.Algorithms/IsSubsequence.cs
@@ -31,7 +31,6 @@ namespace Train.Algorithms
    /// Input: s = "axc", t = "ahbgdc"
    /// Output: false
    ///
-   ///
    /// Constraints:
    ///
    /// 0 <= s.length <= 100
@@ -55,13 +54,81 @@ namespace Train.Algorithms
          return count == s.Length;
       }
       
+      [Test][TestCaseSource(nameof(TestData))]
+      public bool IsSubsequenceBinarySearch(string s, string t)
+      {
+         var map = new List<int>[26];
+         
+         for (var i = 0; i < t.Length; i++)
+         {
+            var index = t[i] - 'a';
+            
+            if(map[index] == null) 
+               map[index] = new List<int>();
+            
+            map[index].Add(i);
+         }
+
+         var prev = -1;
+         foreach (var c in s)
+         {
+            var index = c - 'a';
+
+            if (map[index] == null) return false;
+
+            var candidate = SearchGreaterThen(map[index], prev);
+            if (candidate < 0) return false;
+
+            prev = map[index][candidate];
+         }
+
+         return true;
+      }
+
+      [Test][TestCaseSource(nameof(SearchTestData))]
+      public int SearchGreaterThen(IList<int> list, int target)
+      {
+         int left = 0, right = list.Count - 1, result = -1;
+         
+         while (left <= right)
+         {
+            var mid = left + (right - left) / 2;
+
+            if (list[mid] <= target) {
+               left = mid + 1;
+            } else {
+               result = mid;
+               right = mid - 1;
+            }
+         }
+
+         return result;
+      }
+
       static IEnumerable<TestCaseData> TestData {
          get
          {
             yield return new TestCaseData("", "").Returns(true);
             yield return new TestCaseData("", "abc").Returns(true);
             yield return new TestCaseData("abc", "ahbgdc").Returns(true);
+            yield return new TestCaseData("acb", "ahbgdc").Returns(false);
             yield return new TestCaseData("axc", "ahbgdc").Returns(false);
+         }
+      }
+
+      static IEnumerable<TestCaseData> SearchTestData {
+         get
+         {
+            yield return new TestCaseData(new int[0], 1).Returns(-1);
+            yield return new TestCaseData(new int[]{1}, 1).Returns(-1);
+            yield return new TestCaseData(new int[]{1}, 0).Returns(0);
+            yield return new TestCaseData(new int[]{2}, 1).Returns(0);
+            yield return new TestCaseData(new int[]{1,2}, 1).Returns(1);
+            yield return new TestCaseData(new int[]{1,2,3}, 1).Returns(1);
+            yield return new TestCaseData(new int[]{1,2,3}, 2).Returns(2);
+            yield return new TestCaseData(new int[]{1,2,3,4,5,6,7,8,9}, 8).Returns(8);
+            yield return new TestCaseData(new int[]{1,2,3,4,5,6,7,8,9}, 9).Returns(-1);
+            yield return new TestCaseData(new int[]{1,2,3,4,5,6,7,8,9}, 10).Returns(-1);
          }
       }
    }

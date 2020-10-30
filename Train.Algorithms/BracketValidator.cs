5b95481145bcea4f58942654b205bd51eac7c974
Mikhail Tripolitov <tripolitov@gmail.com>
2020-10-30 19:00:20 +0300

    Solution for Bracket Validator

---
+++ Train.Algorithms/BracketValidator.cs
@@ -0,0 +1,58 @@
+﻿using System.Collections.Generic;
+using NUnit.Framework;
+
+namespace Train.Algorithms
+{
+    /// <summary>
+    /// write a braces/brackets/parentheses validator.
+    /// 
+    /// Let's say:
+    /// 
+    /// '(', '{', '[' are called "openers."
+    /// ')', '}', ']' are called "closers."
+    /// Write an efficient method that tells us whether or not an input string's openers and closers are properly nested.
+    /// 
+    /// Examples:
+    /// 
+    /// "{ [ ] ( ) }" should return true
+    /// "{ [ ( ] ) }" should return false
+    /// "{ [ }" should return false
+    /// </summary>
+    public class BracketValidator
+    {
+        [Test]
+        [TestCaseSource(nameof(TestData))]
+        public static bool IsValid(string code)
+        {
+            var stack = new Stack<char>();
+            foreach (var c in code)
+            {
+                if (c == '(') stack.Push(')');
+                else if (c == '{') stack.Push('}');
+                else if (c == '[') stack.Push(']');
+                else
+                {
+                    if (stack.Count == 0) return false;
+                    if (c != stack.Pop())
+                        return false;
+                }
+            }
+
+            return stack.Count == 0;
+        }
+
+        static IEnumerable<TestCaseData> TestData
+        {
+            get
+            {
+                yield return new TestCaseData("").Returns(true);
+                yield return new TestCaseData("()").Returns(true);
+                yield return new TestCaseData("([]{[]})[]{{}()}").Returns(true);
+                yield return new TestCaseData("([)]").Returns(false);
+                yield return new TestCaseData("([][]}").Returns(false);
+                yield return new TestCaseData("[[]()").Returns(false);
+                yield return new TestCaseData("[[]]())").Returns(false);
+            }
+        }
+    }
+}
